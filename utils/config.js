a42b85c8bc5678730282948c1578f6e17ec68f23
KevinJRQ16 <jhasmani17@gmail.com>
2025-11-05 14:54:48 -0400

    fix

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,13 @@
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// export const JORANI_URL = process.env.JORANI_URL || "http://localhost/session/login";
+// export const HEADLESS = process.env.HEADLESS === "false" ? false : true;
+
 import dotenv from "dotenv";
 dotenv.config();
 
-export const JORANI_URL = process.env.JORANI_URL || "http://localhost/session/login";
+const base = process.env.JORANI_URL || "http://localhost:8080";
+
+export const JORANI_URL = `${base}/session/login`;
 export const HEADLESS = process.env.HEADLESS === "false" ? false : true;
